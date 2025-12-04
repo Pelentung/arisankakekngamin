e2399a1d8f653a6ef5263aa51447e6232a2122d1
Pardi Pardi <pardi.jhr@gmail.com>
2025-12-04 06:46:51 +0000

    FirebaseError: Firebase: Error (auth/api-key-not-valid.-please-pass-a-va

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,10 +2,10 @@
 // Note: This is a public configuration and is safe to expose to the client.
 // Security is enforced by Firestore Security Rules.
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyC_zG5kqIC1imEcDMosB3Iy4me06xHrUh4",
+  authDomain: "simpedak-app.firebaseapp.com",
+  projectId: "simpedak-app",
+  storageBucket: "simpedak-app.appspot.com",
+  messagingSenderId: "55860399091",
+  appId: "1:55860399091:web:7abadb5489c19da4d22086",
 };
